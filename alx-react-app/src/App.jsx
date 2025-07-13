7fe59ba9eed336b9248ae8379a003687793d2fb3
Elliot Nuertey <nuerteyelliot@gmail.com>
2025-07-13 19:39:36 +0000

    new reacts components

--- alx-react-app/src/App.jsx
+++ alx-react-app/src/App.jsx
@@ -4,6 +4,8 @@ import viteLogo from '/vite.svg'
 import './App.css'
 import WelcomeMessage from './components/WelcomeMessage'
 import Header from './components/Header'
+import MainContent from './components/MainContent'
+import Footer from './components/Footer'
 
 function App() {
   const [count, setCount] = useState(0)
@@ -32,6 +34,9 @@ function App() {
       </p>
       <WelcomeMessage />
       <Header />
+      <MainContent />
+      <Footer />
+
     </>
   )
 }
