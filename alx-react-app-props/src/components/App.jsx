7bff35f0ed37a0eb934278dc2d9f9c3c219f292a
Elliot Nuertey <nuerteyelliot@gmail.com>
2025-07-21 17:21:03 +0000

    UserProfile updated

--- alx-react-app-props/src/components/App.jsx
+++ alx-react-app-props/src/components/App.jsx
@@ -1,14 +1,15 @@
 import React from 'react'
 import UserContext from './UserContext'
-import ProfilePage from './ProfilePage';
+import ProfilePage from './ProfilePage'
+
 
 function App() {
   const userData = { name: "Jane Doe", email: "jane.doe@example.com" };
 
-  return( <UserContext.Provider value={userData}></UserContext.Provider>
-  <ProfilePage userData={userData} />;
-
-  ) 
+  return(
+    <UserContext.Provider value={userData}><ProfilePage userData={userData} />
+      <ProfilePage userData={userData} />
+    </UserContext.Provider>) 
 }
 
 export default App;
